ad6ae013fa59ca5d8b2b10ca3ceaab658b1b87a6
SaveZamma <zammatarosalvatore99@gmail.com>
2024-01-15 21:23:12 +0100

    configurato JUnit5 con Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,20 @@
  * Learn more about Gradle by exploring our samples at https://docs.gradle.org/7.4.1/samples
  */
 plugins { java }
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  val junitVersion = "5.9.1"
+  testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+  testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+}
+
+tasks.test {
+  useJUnitPlatform()
+  testLogging { events(TestLogEvent.values()) }
+  testLogging.showStandardStreams = true
+}
