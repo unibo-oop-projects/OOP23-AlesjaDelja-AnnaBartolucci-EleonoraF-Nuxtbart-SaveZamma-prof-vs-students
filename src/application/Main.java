66503c51b347f601f520980ec26cf16ff79f9158
nuxtbart <anna.bartolucci@studio.unibo.it>
2024-01-19 15:51:50 +0100

    Renamed Main.java as App.java and created Main.java class to launch the application

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,48 +1,9 @@
 package application;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.stage.Stage;
-import javafx.util.Duration;
 
-public class Main extends Application {
-	public static MediaPlayer mediaPlayer;
-	
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			addMusic();
-			Parent mainPage=FXMLLoader.load(getClass().getResource("GameView.fxml"));
-	        Scene scene = new Scene(mainPage,1024,600);
-	        primaryStage.setTitle("Prof VS Students");
-	        primaryStage.setScene(scene);
-	        primaryStage.show();
-			
-			/*BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();*/
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void addMusic() {
-        Media sound = new Media(getClass().getResource("/music/background.wav").toString());
-        mediaPlayer = new MediaPlayer(sound);
-        mediaPlayer.setAutoPlay(true);
-        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
-        mediaPlayer.setStartTime(Duration.seconds(0));
-        mediaPlayer.setStopTime(Duration.seconds(50));
-        mediaPlayer.play();
-    }
+public class Main {
 	public static void main(String[] args) {
-		launch(args);
-	}
+		Application.launch(App.class, args);
+		}
 
 }
