46f3fd47b70443c20ca4f8b8b26ba8bdd1641db1
nuxtbart <anna.bartolucci@studio.unibo.it>
2024-02-12 18:55:31 +0100

    added module-info

--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,10 @@
-module module {
+module module{
 	requires javafx.controls;
     requires javafx.fxml;
 	requires javafx.media;
 	requires javafx.graphics;
 	requires javafx.base;
 
+	exports application;
     opens application;
 }
