09561e2a8fa0df45265c839e94566d9ec38263e4
nuxtbart <anna.bartolucci@studio.unibo.it>
2024-02-19 18:46:11 +0100

    minor fix

--- src/module-info.java
+++ src/module-info.java
@@ -2,8 +2,9 @@ module module{
 	requires javafx.controls;
     requires javafx.fxml;
 	requires javafx.media;
-	requires javafx.graphics;
+	//requires javafx.graphics;
 	requires javafx.base;
+	requires transitive javafx.graphics;
 
 	exports application;
     opens application;
