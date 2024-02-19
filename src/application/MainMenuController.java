83f1a9aa4aec567641d5a24f8ffff515025e115d
nuxtbart <anna.bartolucci@studio.unibo.it>
2024-02-19 18:52:37 +0100

    minor fix to start() in MainMenuController

--- src/application/MainMenuController.java
+++ src/application/MainMenuController.java
@@ -13,14 +13,19 @@ public class MainMenuController {
 	@FXML
     private ImageView start;
 	
+	public AnchorPane getMainMenu() {
+        return MainMenu;
+    }
+	
 	@FXML
     void start(MouseEvent event) throws Exception{
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GameView.fxml"));
         AnchorPane pane=fxmlLoader.load();
+        System.out.println("start Game succedded");
         GamePlayView gamePlayView = fxmlLoader.<GamePlayView>getController();
-        //gamePlayView.initialize();
+        
         GamePlayController gamePlayController = new GamePlayController();
-        gamePlayController.initData(gamePlayView); // Inizializza il GamePlayController con il GamePlayView
+        gamePlayController.initData(gamePlayView, MainMenu); // Inizializza il GamePlayController con il GamePlayView
         MainMenu.getChildren().setAll(pane);
     }
 	
