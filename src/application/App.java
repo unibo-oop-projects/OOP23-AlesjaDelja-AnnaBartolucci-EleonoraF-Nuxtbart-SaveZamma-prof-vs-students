8482dbd96e4c7b48e5c15b2030f7f48188a22984
Eleonora_f <eleonora.falconi@studio.unibo.it>
2024-02-24 11:55:57 +0100

    adding changeCharacter function

--- src/application/App.java
+++ src/application/App.java
@@ -11,59 +11,52 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class App extends Application {
-	public static MediaPlayer mediaPlayer;
-	private GridPane gridPane;
+    public static MediaPlayer mediaPlayer;
+    private GridPane gridPane;
     private ProfessorView plantView;
-	
-	@Override
-	public void start(Stage primaryStage) {
-		gridPane = new GridPane();
+
+    @Override
+    public void start(Stage primaryStage) {
+        gridPane = new GridPane();
 
         // Ipotizziamo che tu abbia un'istanza di PlantView chiamata plantView
         // e che imagePath sia il percorso dell'immagine associata alla nuova pianta
-     // Crea un'istanza di PlantView
+        // Crea un'istanza di PlantView
         plantView = new ProfessorView("path_to_default_plant_image");
 
-     // Aggiungi la PlantView alla griglia
+        // Aggiungi la PlantView alla griglia
         gridPane.add(plantView, 0, 0);
 
-     // Aggiungi diversi personaggi alla griglia
-        addCharacter("Character 1", "path_to_character1_image");
-        addCharacter("Character 2", "path_to_character2_image");
-        addCharacter("Character 3", "path_to_character3_image");
+        // Creazione di un'istanza di CharacterChoiceTable e aggiunta alla griglia
+        CharacterChoiceTable characterChoiceTable = new CharacterChoiceTable(plantView);
+        gridPane.add(characterChoiceTable, 1, 0);
 
-     // Imposta un gestore eventi per il cambio del personaggio al clic del mouse
-        plantView.setOnMouseClicked(event -> {
-            // Simula il cambio del personaggio (puoi ottenere questa informazione dalla tua interfaccia utente)
-            String selectedCharacter = "Character 2";  // Cambia questo valore in base alla tua logica di selezione
-            changeCharacter(selectedCharacter);
-        });
-        
         Scene scenes = new Scene(gridPane, 800, 600);
         primaryStage.setScene(scenes);
         primaryStage.show();
-        
-        
-		
-		try {
-			addMusic();
-			Parent mainPage=FXMLLoader.load(getClass().getResource("GameView.fxml"));
-	        Scene scene = new Scene(mainPage,1024,600);
-	        primaryStage.setTitle("Prof VS Students");
-	        primaryStage.setScene(scene);
-	        primaryStage.show();
-			
-			/*BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();*/
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void addMusic() {
+
+        try {
+            addMusic();
+            Parent mainPage = FXMLLoader.load(getClass().getResource("GameView.fxml"));
+            Scene scene = new Scene(mainPage, 1024, 600);
+            primaryStage.setTitle("Prof VS Students");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+            /*
+             * BorderPane root = new BorderPane();
+             * Scene scene = new Scene(root,400,400);
+             * scene.getStylesheets().add(getClass().getResource("application.css").
+             * toExternalForm());
+             * primaryStage.setScene(scene);
+             * primaryStage.show();
+             */
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addMusic() {
         Media sound = new Media(getClass().getResource("/music/background.wav").toString());
         mediaPlayer = new MediaPlayer(sound);
         mediaPlayer.setAutoPlay(true);
@@ -72,8 +65,9 @@ public class App extends Application {
         mediaPlayer.setStopTime(Duration.seconds(50));
         mediaPlayer.play();
     }
-	
-	// Metodo per aggiungere un personaggio alla griglia
+
+    // ELE?
+    // Metodo per aggiungere un personaggio alla griglia
     private void addCharacter(String characterName, String imagePath) {
         ProfessorView characterView = new ProfessorView(imagePath);
         gridPane.add(characterView, 0, 0);
@@ -81,8 +75,9 @@ public class App extends Application {
         // Aggiungi un gestore eventi per il clic del mouse su questo personaggio
         characterView.setOnMouseClicked(event -> changeCharacter(characterName));
     }
-	
-	// Metodo per cambiare il personaggio selezionato
+
+    // ELE
+    // Metodo per cambiare il personaggio selezionato
     private void changeCharacter(String character) {
         // In base al personaggio selezionato, imposta l'immagine corrispondente
         switch (character) {
