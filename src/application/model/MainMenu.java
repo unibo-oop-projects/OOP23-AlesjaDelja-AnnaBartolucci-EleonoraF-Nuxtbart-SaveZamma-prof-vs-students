fbf332919bf2ad1b955dd1d7de83f6354cbff227
Alesja Delja <alesja.delja@studio.unibo.it>
2024-03-01 19:09:10 +0100

    Initial MainMenu, LeaderBoard and CustomeGame

---
+++ src/application/model/MainMenu.java
@@ -0,0 +1,28 @@
+package application.model;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class MainMenu extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("../view/MainMenuView.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	//launch of the main menu of the game
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
