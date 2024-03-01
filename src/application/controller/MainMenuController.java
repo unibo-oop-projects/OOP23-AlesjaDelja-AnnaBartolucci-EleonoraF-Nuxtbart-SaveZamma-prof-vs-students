60180f7ca3ed5ec9c0eabff6407c9171a13a9ced
Alesja Delja <alesja.delja@studio.unibo.it>
2024-03-01 19:07:58 +0100

    Controller that changes Scenes and Initial Main Menu Controller

---
+++ src/application/controller/MainMenuController.java
@@ -0,0 +1,49 @@
+package application.controller;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+
+public class MainMenuController {
+	
+	/**
+	 * Starts a default game
+	 * @param e Listens to when the player click on this buttons
+	 */
+	
+	public void newGame(ActionEvent e) {
+		System.out.println("New Game");
+	}
+	
+	/**
+	 * Changes to the view when the player can decide how he/she wants to play and it's difficulty
+	 * @param e Listens to when the player click on this buttons
+	 */
+	public void costumeGame(ActionEvent e) throws IOException {
+		StageChangeController stageChanger = new StageChangeController();
+        stageChanger.changeScene(e, "../view/CustomeGameView.fxml");
+	}
+	
+	/**
+	 * When The player click on it it changes the sound from on to off and from off to on.
+	 * In default this is set to ON
+	 * @param e Listens to when the player click on this buttons
+	 */
+	public void sounds(ActionEvent e) {
+		System.out.println("Sounds");
+	}
+	//TO-DO
+	public void leaderboard(ActionEvent e) {
+		System.out.println("Leaderboard");
+	}
+	/**
+	 * Closes the game when clicked
+	 * @param e Listens to when the player click on this buttons
+	 */
+	public void exitGame(ActionEvent e) {
+		System.out.println("Exit Game");
+	}
+	
+}
+
+
---
+++ src/application/controller/StageChangeController.java
@@ -0,0 +1,29 @@
+package application.controller;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class StageChangeController {
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	/**
+	 * It helps changing from a scene to another one without having to do in every single time
+	 * @param e Listens to when the player click on this buttons
+	 * @param nameScene The location of the new FXML file. It has always to start from application.view.NameOfTheFile
+	 * @throws IOException
+	 */
+	public void changeScene(ActionEvent e, String nameScene) throws IOException {
+		root = FXMLLoader.load(getClass().getResource(nameScene));
+		stage = (Stage) ((Node)e.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+}
