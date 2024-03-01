1913d1a0dae249b04f6eb6c717b09d6021e35874
Alesja Delja <alesja.delja@studio.unibo.it>
2024-03-01 17:36:57 +0100

    Added example of how energy is going to be used

---
+++ src/test/resources/Energy.java
@@ -0,0 +1,38 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+interface Energy {
+    void incrementaConPassareDelTempo();
+}
+
+class EnergiaImplementazione implements Energy {
+    private int livelloEnergia;
+
+    public EnergiaImplementazione() {
+        this.livelloEnergia = 0;
+        avviaIncrementoPeriodico();
+    }
+
+    @Override
+    public void incrementaConPassareDelTempo() {
+        livelloEnergia++;
+        System.out.println("Livello di energia incrementato a " + livelloEnergia);
+    }
+
+    
+    //incremento dell'enegia ogni minuto che passa
+    private void avviaIncrementoPeriodico() {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                incrementaConPassareDelTempo();
+            }
+        }, 0, 60000); // 60000 millisecondi = 1 minuto
+    }
+    
+    //TO-DO metodo che fa incrementare l'energia in game avanzato quando i prof uccidono gli studenti
+    //TO-DO metodo che fa diminuire l'energia in game avanzato quando gli studenti uccidono i prof
+    //TO-DO metodo che fa diminuire l'enrgia a seconda dei diversi spawn dei prof
+    
+}
