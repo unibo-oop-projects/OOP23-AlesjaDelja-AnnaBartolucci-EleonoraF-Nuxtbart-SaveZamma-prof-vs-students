a7cb47112cacef9ce9ca7578fea9442f15a04f19
Eleonora_f <eleonora.falconi@studio.unibo.it>
2024-03-09 14:35:25 +0100

    created classes

---
+++ src/main/java/_OOP_develop_gradle/Bullet.java
@@ -0,0 +1,48 @@
+package _OOP_develop_gradle;
+
+public final class Bullet {
+
+    private Elements<Integer, Integer> currentPosition;
+    private final int bulletSpeed;
+    private final double bulletDamage;
+    private final String bulletName;
+
+    /**
+     * Constructor for BulletEntity.
+     * 
+     * @param speed  the speed of the bullet
+     * @param damage the damage of the bullet
+     * @param pos    the position of the bullet
+     * @param name   the name of the bullet
+     */
+    public Bullet(int speed, double damage, Elements<Integer, Integer> pos, String name) {
+        this.bulletSpeed = speed;
+        this.bulletDamage = damage;
+        this.currentPosition = pos;
+        this.bulletName = name;
+    }
+
+    public Elements<Integer, Integer> getPosition() {
+        return currentPosition;
+    }
+
+    public boolean isAlive() {
+        return true;
+    }
+
+    public String getBulletName() {
+        return this.bulletName;
+    }
+
+    public int getBulletSpeed() {
+        return this.bulletSpeed;
+    }
+
+    public double getBulletDamage() {
+        return this.bulletDamage;
+    }
+
+    public void move() {
+        this.currentPosition = new Elements<>(currentPosition.getX() + bulletSpeed, currentPosition.getY());
+    }
+}
