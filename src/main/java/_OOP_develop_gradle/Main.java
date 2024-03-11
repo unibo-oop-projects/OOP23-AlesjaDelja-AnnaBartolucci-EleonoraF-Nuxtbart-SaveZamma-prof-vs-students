cc79453a4d46e4669a4f67c478b76da500df4216
Alelover01 <76488337+Alelover01@users.noreply.github.com>
2024-03-11 16:27:39 +0100

    Changed The Start Of The Game

--- src/main/java/_OOP_develop_gradle/Main.java
+++ src/main/java/_OOP_develop_gradle/Main.java
@@ -1,10 +1,11 @@
 package _OOP_develop_gradle;
 
+import _OOP_develop_gradle.model.MainMenu;
 import javafx.application.Application;
 
 public class Main {
 	public static void main(String[] args) {
-		Application.launch(App2.class, args);
+		Application.launch(MainMenu.class, args);
 		}
 
 }
--- src/main/java/_OOP_develop_gradle/model/MainMenu.java
+++ src/main/java/_OOP_develop_gradle/model/MainMenu.java
@@ -20,9 +20,4 @@ public class MainMenu extends Application {
 		}
 	}
 	
-	// DA CAMBIARE E FARLO PARTIRE DA QUI IL GIOCO
-	//launch of the main menu of the game
-	//public static void main(String[] args) {
-		//launch(args);
-	//}
 }
