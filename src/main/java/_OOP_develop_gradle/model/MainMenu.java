4c2a8a18e7f825b790a566d15c3a91a49d123537
Alelover01 <76488337+Alelover01@users.noreply.github.com>
2024-03-11 17:02:18 +0100

    Updated exit status

--- src/main/java/_OOP_develop_gradle/model/MainMenu.java
+++ src/main/java/_OOP_develop_gradle/model/MainMenu.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Alert.AlertType;
 
 
 public class MainMenu extends Application {
@@ -15,9 +18,26 @@ public class MainMenu extends Application {
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			primaryStage.setOnCloseRequest(event -> {
+				//so when we click on cancel it return to the main menu
+				event.consume();
+				exitGame(primaryStage);
+				});
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	public void exitGame(Stage stage) {
+		
+		//Insert an alert that makes sure that the user is sure about exiting the game
+		Alert exitAlert = new Alert(AlertType.CONFIRMATION);
+		exitAlert.setTitle("Exit");
+		exitAlert.setHeaderText("You are going to exit the game!");
+		exitAlert.setContentText("Are you sure?");
+		if(exitAlert.showAndWait().get() == ButtonType.OK) {
+			stage.close();
+		}
+	}
 }
