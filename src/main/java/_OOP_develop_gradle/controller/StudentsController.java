a6d419f60cd96ab0649661ca70babd7d5a65bb0a
Alelover01 <76488337+Alelover01@users.noreply.github.com>
2024-03-11 19:22:32 +0100

    Updarte of the random spawn of a student

--- src/main/java/_OOP_develop_gradle/controller/StudentsController.java
+++ src/main/java/_OOP_develop_gradle/controller/StudentsController.java
@@ -1,13 +1,21 @@
 package _OOP_develop_gradle.controller;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.animation.AnimationTimer;
+
+import java.io.IOException;
 import java.util.Random;
+
+import application.controller.StageChangeController;
 
 public class StudentsController extends Application {
 
@@ -80,6 +88,30 @@ public class StudentsController extends Application {
         block.relocate(blockX * CELL_SIZE, blockY * CELL_SIZE);
     } */
 	
+	@FXML
+	private GridPane gridPane;
+
+
+	public void back(ActionEvent e) throws IOException {
+		StageChangeController stageChanger = new StageChangeController();
+        stageChanger.mainMenu(e);
+	}
+
+	    public void initialize() {
+	        try {
+	            // Carica l'elemento FXML 	imparare come fare quello ma in un'altra classe
+	            FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/Student.fxml"));
+	            Pane studentPane = loader.load();
+	            //posizione Random dello studente
+	            int rowIndex = new Random().nextInt(6); 
+	            // Aggiungi l'elemento FXML alla GridPane
+	            gridPane.add(studentPane, 8, rowIndex); // Ad esempio, aggiungi l'elemento FXML alla colonna 0 e riga 0
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	
+	
 	public void showStudents() {
 		//TO DO Aggiungere la parte in cui si rende visibile lo studente
 	}
@@ -89,5 +121,5 @@ public class StudentsController extends Application {
 		// TODO Auto-generated method stub
 		
 	}
-	
+
 }
