fa4395c7d70bf4ffd331a9c5f235d2735251b9fa
Alelover01 <76488337+Alelover01@users.noreply.github.com>
2024-03-11 18:01:19 +0100

    Create HelpGameController.java

---
+++ src/main/java/_OOP_develop_gradle/controller/HelpGameController.java
@@ -0,0 +1,15 @@
+package _OOP_develop_gradle.controller;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+
+public class HelpGameController {
+
+	
+	public void back(ActionEvent e) throws IOException {
+		StageChangeController stageChanger = new StageChangeController();
+        stageChanger.mainMenu(e);
+	}
+	
+}
