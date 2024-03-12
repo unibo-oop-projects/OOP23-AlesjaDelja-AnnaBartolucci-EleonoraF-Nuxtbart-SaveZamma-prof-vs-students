73b6e489c6d63ead09f2c2773dd919362d5f3bbd
Alelover01 <76488337+Alelover01@users.noreply.github.com>
2024-03-12 19:34:15 +0100

    Generate random Position for the student +  set/get di pathStudentImg

--- src/main/java/_OOP_develop_gradle/model/Student.java
+++ src/main/java/_OOP_develop_gradle/model/Student.java
@@ -9,25 +9,28 @@ public class Student {
 	private int damage;
 	private int DEFAULT_HEALTH = 100;
 	private int DEFAULT_DAMAGE = 25;
-	privaye int DEFAULT_SCORE = 100;
-	private final Elements<Integer, Integer> position;
+	private int DEFAULT_SCORE = 100;
+	private String pathStudentImg;
+	private Elements<Integer, Integer> positionStudent;
 	//Costruttore
-	public Student() {
-		this.position=getPosition();
+	public Student(String pathStudentImg) {
+		this.pathStudentImg = pathStudentImg;
 		this.health = DEFAULT_HEALTH;
 		this.damage = DEFAULT_DAMAGE;
+		generateRandomPosition();
 	}
+	
 	
 	/**
 	 * Creates a new Students in the end of the grid in a random row
 	 * @return the rows and colum of the student created
 	 */
 	
-	private Elements<Integer, Integer> getPosition() {
-		Random random = new Random();
-	    int randomY = random.nextInt(7); // Genera un numero casuale tra 0 e 6 inclusi
-	    return new Elements<>(0, randomY);
-	}
+	private void generateRandomPosition() {
+        Random random = new Random();
+        int randomY = random.nextInt(7);
+        this.positionStudent = new Elements<>(0, randomY);
+    }
 
 	//Now we make the method that gets and sets the health and damage
 	/**
@@ -65,10 +68,9 @@ public class Student {
 	 * @return The new Score after the student is Killed
 	 */
 	
-	public int destroyStudents(int score) {
-		//TO-DO Distruzione 
+	public int destroyStudents(int score) { 
 		int newScore = score + DEFAULT_SCORE;
-		//distruggere immagine
+		// TODO distruggere immagine
 		return newScore;
 	}
 	
@@ -81,4 +83,37 @@ public class Student {
 	public void takeDamageStudents(int damageTaken) {
 		health -= damageTaken;
 	}
+	/**
+	 * Gets the position of the Students
+	 * @return the current position of the student
+	 */
+	public Elements<Integer, Integer> getPositionStudent() {
+		return positionStudent;
+	} 
+	/**
+	 * Sets the new Position of the Student
+	 * @param position Sets the new position of the student
+	 */
+	public void setPositionStudent(Elements<Integer, Integer> position) {
+        this.positionStudent = position;
+    }
+
+
+	public String getPathStudentImg() {
+		return pathStudentImg;
+	}
+
+
+	public void setPathStudentImg(String pathStudentImg) {
+		this.pathStudentImg = pathStudentImg;
+	}
+	/* TODO Crearlo anche per lo studente
+	 * private ImageView createImageView() { ImageView image = new ImageView(new
+	 * Image(path)); image.setFitWidth(50); // Imposta la larghezza desiderata
+	 * image.setFitHeight(50); // Imposta l'altezza desiderata return image; }
+	 */
+	/* Metodo per ottenere l'ImageView della pianta
+    public ProfessorView getImageProf(Professor prof) {
+    	return new ProfessorView(pathImg);
+    }*/
 }
