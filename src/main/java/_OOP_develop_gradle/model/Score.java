5e308f63f30cd1576f6705299722015ce7fcc18d
Alelover01 <76488337+Alelover01@users.noreply.github.com>
2024-03-13 19:36:51 +0100

    Update Score

--- src/main/java/_OOP_develop_gradle/model/Score.java
+++ src/main/java/_OOP_develop_gradle/model/Score.java
@@ -1,17 +1,45 @@
 package _OOP_develop_gradle.model;
 
+import javafx.scene.control.Label;
+
 public class Score {
+	
+	private int score;
+	
 
 	private int DEFAULT_SCORE = 100;
 
 	/**
-	 * Add the score when a students dies
-	 * @param score The score at the moment
-	 * @return The new Score after the student is Killed
+	 * Getter of score
+	 * @return the current score
+	 */
+	public int getScore() {
+		return score;
+	}
+
+	/**
+	 * Sets the score
+	 * @param score Sets the new score
+	 */
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	/**
+	 * Adds the score to the current one and updates the label with the new Score
+	 * @param scoreLabel The label that needs to be updated
 	 */
 	
-	public int addScore(int score) { 
-		int newScore = score + DEFAULT_SCORE;
-		return newScore;
+	public void addScore(Label scoreLabel) { 
+		this.score = score +  DEFAULT_SCORE;
+		updateScore(scoreLabel);
+	}
+	
+	/**
+	 * Updates the score
+	 * @param scoreLabel The label that needs to be updated
+	 */
+	public void updateScore(Label scoreLabel) {
+		scoreLabel.setText("Score: " + score);
 	}
 }
