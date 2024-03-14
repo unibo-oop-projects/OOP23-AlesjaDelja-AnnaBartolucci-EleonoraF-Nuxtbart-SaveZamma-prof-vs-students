e0ded3eef8bc80f534af9bdc9eee984b864997f9
Eleonora_f <eleonora.falconi@studio.unibo.it>
2024-03-14 15:11:21 +0100

    creating elements javadoc

--- src/main/java/_OOP_develop_gradle/Elements.java
+++ src/main/java/_OOP_develop_gradle/Elements.java
@@ -11,12 +11,23 @@ public class Elements<X, Y> {
 	 private final X x;
 	    private final Y y;
 
+	    /**
+	     * Constructs a pair of elements.
+	     * 
+	     * @param x The first element.
+	     * @param y The second element.
+	     */
 	    public Elements(final X x, final Y y) {
 	        this.x = x;
 	        this.y = y;
 			
 	    }
 
+	    /**
+	     * Returns the first element of the pair.
+	     * 
+	     * @return The first element of the pair.
+	     */
 	    public X getX() {
 	        return this.x;
 	    }
@@ -30,11 +41,22 @@ public class Elements<X, Y> {
 	        return this.y;
 	    }
 
+	    /**
+	     * Computes a hash code for this pair.
+	     * 
+	     * @return A hash code value for this pair.
+	     */
 	    @Override
 	    public int hashCode() {
 	        return Objects.hash(this.x, this.y);
 	    }
 	    
+	    /**
+	     * Indicates whether some other object is "equal to" this one.
+	     * 
+	     * @param obj The reference object with which to compare.
+	     * @return true if this pair is the same as the obj argument; false otherwise.
+	     */
 	    @SuppressWarnings("rawtypes")
 	    @Override
 	    public boolean equals(final Object obj) {
@@ -51,6 +73,11 @@ public class Elements<X, Y> {
 	        return Objects.equals(this.x, other.x) && Objects.equals(this.y, other.y);
 	    }
 
+	    /**
+	     * Returns a string representation of the pair.
+	     * 
+	     * @return A string representation of the pair.
+	     */
 	    @Override
 	    public String toString() {
 	        return "Pair [X = " + this.x + ", Y = " + this.y + "]";
