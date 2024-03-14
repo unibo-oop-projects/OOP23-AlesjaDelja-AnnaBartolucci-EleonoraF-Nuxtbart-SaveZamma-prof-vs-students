a11c2de78d79e5cc70fe9c234a2369d75b456605
Eleonora_f <eleonora.falconi@studio.unibo.it>
2024-03-14 13:01:51 +0100

    adding javadocs to bullet

--- src/main/java/_OOP_develop_gradle/Bullet.java
+++ src/main/java/_OOP_develop_gradle/Bullet.java
@@ -29,6 +29,10 @@ public final class Bullet {
         this.pathImgB = pathImgB;
     }
 
+    /**
+	 * Gets the position of the Bullets
+	 * @return the current position of the Bullets
+	 */
     public Elements<Integer, Integer> getPosition() {
         return currentPosition;
     }
@@ -37,22 +41,38 @@ public final class Bullet {
         return true;
     }
 
+    /**
+	 * Gets the name of the Bullet
+	 * @return the current name of the Bullet
+	 */
     public String getBulletName() {
         return this.bulletName;
     }
 
+    /**
+	 * Gets the speed of the Bullet
+	 * @return the current speed of the Bullet
+	 */
     public int getBulletSpeed() {
         return this.bulletSpeed;
     }
 
+    /**
+	 * Gets the damage of the Bullet
+	 * @return the current damage2 of the Bullet
+	 */
     public int getBulletDamage() {
         return this.bulletDamage;
     }
 
+    /**
+	 * Updates the position when he the bullet is fired.
+	 * @return the position after shooting
+	 */
     public void move() {
         this.currentPosition = new Elements<>(currentPosition.getX() + bulletSpeed, currentPosition.getY());
     }
-    
+     //POSSO ELIMINARLO?
     public String getPathImgBullet() {
 		return pathImgB;
 	}
