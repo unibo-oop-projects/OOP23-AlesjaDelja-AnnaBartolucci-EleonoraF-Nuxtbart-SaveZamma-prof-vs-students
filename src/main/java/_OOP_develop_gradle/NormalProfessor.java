1e23e243f464a5c2e25011d80ddfc384c6ed7502
Eleonora_f <eleonora.falconi@studio.unibo.it>
2024-03-14 14:36:54 +0100

    creating NormalProf javadoc

--- src/main/java/_OOP_develop_gradle/NormalProfessor.java
+++ src/main/java/_OOP_develop_gradle/NormalProfessor.java
@@ -14,30 +14,53 @@ public class NormalProfessor extends Professor{
 	public int bulletSpeed=2;
 	private String pathImgNP;
 	
+	/**
+	 * Constructor for creating a NormalProfessor object.
+	 *
+	 * @param col The column position of the NormalProfessor.
+	 * @param row The row position of the NormalProfessor.
+	 */
 	public NormalProfessor(int col, int row) {
 		super(NORMALPROF_HIT_DAMAGE, NORMALPROF_HEALTHPOINTS, new Elements<Integer, Integer>(col, row), NORMALPROF_IMG_PATH, NORMALPROF_BUY_COST, NormalProfName);
 		normalProfBullet = new Bullet(bulletSpeed, NORMALPROF_HIT_DAMAGE, new Elements<Integer, Integer>(col, row), tutorBulletName, pathImgNP);
 	}
-/**
- * 
- * @return
- */
+
+	/**
+	 * Gets the Bullet of a Tutor
+	 * @return the Bullet of the Tutor
+	 */
 	public Bullet getTutorBullet() {
 		return normalProfBullet;
 	}
 
+	/**
+	 * Sets the Bullet of a Tutor
+	 * @param Bullet Value of the Tutor
+	 */
 	public void setTutorBullet(Bullet tutorBullet) {
 		this.normalProfBullet = tutorBullet;
 	}
 
+	/**
+	 * Gets the BulletSpeed of a Tutor
+	 * @return the BulletSpeed of the Tutor
+	 */
 	public int getBulletSpeed() {
 		return bulletSpeed;
 	}
 
+	/**
+	 * Sets the BulletSpeed of a Tutor
+	 * @param BulletSpeed Value of the Tutor
+	 */
 	public void setBulletSpeed(int bulletSpeed) {
 		this.bulletSpeed = bulletSpeed;
 	}
 
+	/**
+	 * Gets the BulletName of a Tutor
+	 * @return the BulletName of the Tutor
+	 */
 	public static String getTutorbulletname() {
 		return tutorBulletName;
 	}
