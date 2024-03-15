1e2c6f7398e1da117f38d45d27f010c2f5259da0
Eleonora_f <eleonora.falconi@studio.unibo.it>
2024-03-15 23:48:36 +0100

    fixing

--- src/main/java/_OOP_develop_gradle/view/BulletView.java
+++ src/main/java/_OOP_develop_gradle/view/BulletView.java
@@ -12,5 +12,19 @@ public class BulletView  extends ElementView{
     protected String getImagePath() {
 		return "/img/pea.png"; 
     }
+	
+	// Metodo per ottenere un'immagine ridimensionata
+    private Image getResizedImage() {
+        Image image = new Image(getImagePath());
+        // Definisci la nuova larghezza e altezza desiderata per l'immagine
+        double newWidth = 20; // Nuova larghezza desiderata
+        double newHeight = 20; // Nuova altezza desiderata
+        // Crea un'ImageView per l'immagine e imposta le nuove dimensioni
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(newWidth);
+        imageView.setFitHeight(newHeight);
+        // Restituisci l'immagine ridimensionata
+        return imageView.snapshot(null, null);
+    }
 
 }
