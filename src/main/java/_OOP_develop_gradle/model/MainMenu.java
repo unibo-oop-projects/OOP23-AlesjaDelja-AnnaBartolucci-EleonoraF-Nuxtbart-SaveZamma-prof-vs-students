bbfca386844021c5d9d3b178ce3cb6a240ce5c02
Alelover01 <76488337+Alelover01@users.noreply.github.com>
2024-03-16 15:39:55 +0100

    Added Inteface

--- src/main/java/_OOP_develop_gradle/model/MainMenu.java
+++ src/main/java/_OOP_develop_gradle/model/MainMenu.java
@@ -1,44 +1,9 @@
 package _OOP_develop_gradle.model;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
-public class MainMenu extends Application {
-		@Override
-		public void start(Stage primaryStage) {
-			try {
-				System.out.println(getClass().getResource("/MainMenuView.fxml"));
-			    //Parent root = FXMLLoader.load(ClassLoader.getSystemResource("MainMenuView.fxml"));
-				Parent root = FXMLLoader.load(getClass().getResource("/MainMenuView.fxml"));
-				Scene scene = new Scene(root);
-				primaryStage.setScene(scene);
-				primaryStage.show();
-				primaryStage.setOnCloseRequest(event -> {
-					//so when we click on cancel it return to the main menu
-					event.consume();
-					exitGame(primaryStage);
-					});
-				
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
-		
-		public void exitGame(Stage stage) {
-			
-			//Insert an alert that makes sure that the user is sure about exiting the game
-			Alert exitAlert = new Alert(AlertType.CONFIRMATION);
-			exitAlert.setTitle("Exit");
-			exitAlert.setHeaderText("You are going to exit the game!");
-			exitAlert.setContentText("Are you sure?");
-			if(exitAlert.showAndWait().get() == ButtonType.OK) {
-				stage.close();
-			}
-		}
-	}
+public interface MainMenu {
+	    void start(Stage primaryStage);
+	    void exitGame(Stage stage);
+
+}
