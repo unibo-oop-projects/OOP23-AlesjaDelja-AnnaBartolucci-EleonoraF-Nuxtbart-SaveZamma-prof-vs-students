8b6e028f502a420a4764d8b55782043e8f41fe78
Eleonora_f <eleonora.falconi@studio.unibo.it>
2024-03-19 11:32:28 +0100

    fixing Elements

--- src/main/java/_OOP_develop_gradle/model/Elements.java
+++ src/main/java/_OOP_develop_gradle/model/Elements.java
@@ -20,27 +20,27 @@ public class Elements<X, Y> {
 	    }
 
 	    /**
-	     * Returns the first element of the pair.
+	     * Returns the first element of the element.
 	     * 
-	     * @return The first element of the pair.
+	     * @return The first element of the element.
 	     */
 	    public X getX() {
 	        return this.x;
 	    }
 
 	    /**
-	     * Returns the second element of the Pair Object.
+	     * Returns the second element of the Element Object.
 	     * 
-	     * @return second element of pair.
+	     * @return second element of elements.
 	     */
 	    public Y getY() {
 	        return this.y;
 	    }
 
 	    /**
-	     * Computes a hash code for this pair.
+	     * Computes a hash code for this elements.
 	     * 
-	     * @return A hash code value for this pair.
+	     * @return A hash code value for this elements.
 	     */
 	    @Override
 	    public int hashCode() {
@@ -48,31 +48,44 @@ public class Elements<X, Y> {
 	    }
 	    
 	    /**
-	     * Indicates whether some other object is "equal to" this one.
-	     * 
-	     * @param obj The reference object with which to compare.
-	     * @return true if this pair is the same as the obj argument; false otherwise.
+	     * Checks if the specified object is equal to this object.
+	     * Two objects are considered equal if:
+	     * <ul>
+	     *     <li>They are the same reference object.</li>
+	     *     <li>The specified object is not null and has the same type as this instance.</li>
+	     *     <li>Both objects' {@code x} and {@code y} properties are equal.</li>
+	     * </ul>
+	     *
+	     * @param obj the object to be compared with this instance
+	     * @return {@code true} if the specified object is equal to this instance; {@code false} otherwise
 	     */
 	    @SuppressWarnings("rawtypes")
-	    @Override
-	    public boolean equals(final Object obj) {
-	        if (this == obj) {
-	            return true;
-	        }
-	        if (obj == null) {
-	            return false;
-	        }
-	        if (getClass() != obj.getClass()) {
-	            return false;
-	        }
-	        final Elements other = (Elements) obj;
-	        return Objects.equals(this.x, other.x) && Objects.equals(this.y, other.y);
-	    }
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Elements other = (Elements) obj;
+			if (x == null) {
+				if (other.x != null)
+					return false;
+			} else if (!x.equals(other.x))
+				return false;
+			if (y == null) {
+				if (other.y != null)
+					return false;
+			} else if (!y.equals(other.y))
+				return false;
+			return true;
+		}
 
 	    /**
-	     * Returns a string representation of the pair.
+	     * Returns a string representation of the element.
 	     * 
-	     * @return A string representation of the pair.
+	     * @return A string representation of the element.
 	     */
 	    @Override
 	    public String toString() {
