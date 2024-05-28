34b8a787aadafd32eee1d2425489275aba1cdf12
Anna Bartolucci <anna.bartolucci@studio.unibo.it>
2024-05-28 22:09:13 +0200

    minor fix in order to have a correct chekstyle

--- src/main/java/oopdevelopgradle/controller/HelpGameController.java
+++ src/main/java/oopdevelopgradle/controller/HelpGameController.java
@@ -11,13 +11,18 @@ public class HelpGameController implements GameControllerInterface {
     @FXML
     private Label helpLabel; 
     
-    private String TEST_LABEL ="Prof vs. Students is a strategy and defense game where the player must protect their house from waves of students using a variety of profs with special abilities.\r\n"
-    		+ "Players can plant various types of profs each with unique defensive functions such as shooting direct and diagonal projectiles . \r\n"
-    		+ "During the game, players must strategically plan the placement of prof and manage resources (energy) to plant new defenses. \r\n"
-    		+ "The player MUST choose the first time a prof and then where to plant it (where there isn't anything) on the grid, if he doesn't choose a different prof then he will plant the last choosen prof.\r\n"
+    private String testLabel = "Prof vs. Students is a strategy and defense game where the player "
+    		+ "must protect their house from waves of students using a variety of profs with special abilities.\r\n"
+    		+ "Players can plant various types of profs each with unique defensive functions such as shooting direct "
+    		+ "and diagonal projectiles . \r\n"
+    		+ "During the game, players must strategically plan the placement of prof and manage resources (energy)"
+    		+ " to plant new defenses. \r\n"
+    		+ "The player MUST choose the first time a prof and then where to plant it (where there isn't anything) "
+    		+ "on the grid, if he doesn't choose a different prof then he will plant the last choosen prof.\r\n"
     		+ "If there isn't enough energy, the player can't plant the prof in the grid.\r\n"
     		+ "Students will come from different directions.\r\n"
-    		+ "The ultimate goal is to survive for as a minute, protecting your university and preventing the students from reaching the final goal.\r\n"
+    		+ "The ultimate goal is to survive for as a minute, protecting your university and preventing the students"
+    		+ " from reaching the final goal.\r\n"
     		+ "\r\n"
     		+ "LEGENDS:\r\n"
     		+ "Student-> Health 100 - Damage 25\r\n"
@@ -29,10 +34,10 @@ public class HelpGameController implements GameControllerInterface {
     		+ "    Rector-> Health 150 - Damage 50 - Energy 30 - Shoots diagonal projectiles";
 	
     public void initialize() throws IOException {
-		helpLabel.setText(TEST_LABEL);
+		helpLabel.setText(testLabel);
     }
     @Override
-    public void back(ActionEvent e) throws IOException {
+    public void back(final ActionEvent e) throws IOException {
         StageChangeController stageChanger = new StageChangeController();
         stageChanger.mainMenu(e);
     }
